c59186e8188650c671fb53067c182b111083c54f
YeOldeDM <nines@email.com>
2017-06-01 19:53:51 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -777,7 +777,7 @@ IMAGE_FOLDERS = {'images': 'images'}
 # vs
 # xcode
 # This list MAY be incomplete since pygments adds styles every now and then.
-CODE_COLOR_SCHEME = 'python'
+# CODE_COLOR_SCHEME = ''
 
 # If you use 'site-reveal' theme you can select several subthemes
 # THEME_REVEAL_CONFIG_SUBTHEME = 'sky'
